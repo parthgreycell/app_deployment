0e278ccfa112e834feff07a8f5439881c21aa627
parthgreycell <parthchauhan.egreycell@gmail.com>
2023-02-10 17:53:46 +0530

    files

--- nginx/nginx.groovy
+++ nginx/nginx.groovy
@@ -7,11 +7,10 @@ node{
       dir('parth') {
         checkout poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'git_token', url: 'git@github.com:parthgreycell/app_deployment.git']]]
         sh """        
-        cd nginx
         ls
         # docker build --file=Dockerfile --tag=nginx:${PUBLISHTAG} nginx/
         pwd
-        docker build -t nginximg:${PUBLISHTAG} Dockerfile
+        docker build -t nginximg:${PUBLISHTAG} nginx/
         docker images
          export AWS_PROFILE=default
   sudo aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 561279971319.dkr.ecr.us-east-1.amazonaws.com 
