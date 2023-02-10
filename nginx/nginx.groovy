b8fc2b774b8b32116207f01683f49fca99549ef8
parthgreycell <parthchauhan.egreycell@gmail.com>
2023-02-10 13:29:27 +0530

    nginx

--- nginx/nginx.groovy
+++ nginx/nginx.groovy
@@ -1,11 +1,6 @@
 node{
   try{
-    properties([
-      parameters([
-        [$class: 'ListSubversionTagsParameterDefinition', credentialsId: 'munjal-gc-un-pw', defaultValue: '', maxTags: '', name: 'TagName', reverseByDate: true, reverseByName: false, tagsDir: 'https://github.com/BidClips/BidClips-API-Restheart.git', tagsFilter: '']
-      ])
-    ])
-    def PUBLISHTAG = ""
+    def PUBLISHTAG = """sh date +%Y_%m_%d-%H_%M_%S-%A"""
     def repoRegion = ""
 
     stage('Preparation'){
@@ -14,34 +9,21 @@ node{
         currentBuild.result = 'FAILURE'
         throw new RuntimeException("required parameter missing : ${TagName}");
       }
-      dir('BidClips-API-Restheart') {
-        if (TagName.equals('trunk')) {
-          TagName = 'branches/master'
-          checkout poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'munjal-gc', url: 'git@github.com:BidClips/BidClips-API-Restheart.git']]]
-          PUBLISHTAG = sh(
-            script: 'echo $(git log -1 --pretty=%h)',
-            returnStdout: true
-          ).trim()
-          repoRegion = "us-east-1"
-
-        }
-      }
     }
-    }
-
+    
     stage('Building Docker Image'){
-      dir('BidClips-API-Restheart') {
+      // dir('BidClips-API-Restheart') {
         sh """
-        docker build --file=Dockerfile --tag=bidclips-api-restheart:${PUBLISHTAG} .
+        docker build --file=Dockerfile --tag=nginx:${PUBLISHTAG} nginx/.
         """
-      }
+      // }
     }
+
     stage("Publishing ${PUBLISHTAG}"){
         sh """
-  // export AWS_PROFILE=bidclips-eks
   aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 561279971319.dkr.ecr.us-east-1.amazonaws.com
-  docker tag bidclips-api-restheart:${PUBLISHTAG} 561279971319.dkr.ecr.us-east-1.amazonaws.com/bidclips-api-restheart:${PUBLISHTAG}
-  docker push 561279971319.dkr.ecr.us-east-1.amazonaws.com/bidclips-api-restheart:${PUBLISHTAG}
+  docker tag nginx:${PUBLISHTAG} 561279971319.dkr.ecr.us-east-1.amazonaws.com/nginx:${PUBLISHTAG}
+  docker push 561279971319.dkr.ecr.us-east-1.amazonaws.com/nginx:${PUBLISHTAG}
           """
     }
     stage('Cleanup'){
@@ -57,6 +39,7 @@ node{
       }
     }
   }
+  
   catch( exec ) {
     echo "FAILURE: ${exec}"
     currentBuild.result = 'FAILURE'
@@ -74,5 +57,5 @@ node{
     else {
       echo 'One way or another, I have finished'
     }
-  }
+}
 }
