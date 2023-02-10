be516548f3a7fea79d7d509a1f9f5b0a1da0a7e0
parthgreycell <parthchauhan.egreycell@gmail.com>
2023-02-10 16:00:31 +0530

    nginx

--- nginx/nginx.groovy
+++ nginx/nginx.groovy
@@ -14,7 +14,7 @@ node{
 
     stage("Publishing ${PUBLISHTAG}"){
         sh """
-  export AWS_PROFILE=parth
+  export AWS_PROFILE=default
   aws sts get-caller-identity
   aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 561279971319.dkr.ecr.us-east-1.amazonaws.com
   docker tag nginx:${PUBLISHTAG} 561279971319.dkr.ecr.us-east-1.amazonaws.com/nginx:${PUBLISHTAG}
